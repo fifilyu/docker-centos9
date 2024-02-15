5da3add7193bb8b6d16f3925e873342530d236d8
Fifi Lyu <fifilyu@gmail.com>
2024-02-15 23:14:58 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 tag=$(date +'%Y%m%d-%H%M%S')
 
-docker images | grep -E "^fifilyu/docker-centos9   ${tag}"
+docker images | grep -E "^fifilyu/centos9   ${tag}"
 
 if [ $? -eq 0 ]; then
     echo "[错误] 指定的Docker镜像tag已经存在：${tag}"
     exit 1
 fi
 
-echo "[信息] 构建Docker镜像：fifilyu/docker-centos9:${tag}"
-docker buildx build -t fifilyu/docker-centos9:${tag} .
+echo "[信息] 构建Docker镜像：fifilyu/centos9:${tag}"
+docker buildx build -t fifilyu/centos9:${tag} .
